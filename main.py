4f98bc05c0f633ae7355cc581bd032a122388de9
Tiche12 <stshimanga1@ivytech.edu>
2024-04-25 11:36:56 -0400

    added to Github

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print ('hello world to')
+class Restaurant:
+    def __init__(self):
+        self.time_slot = {}
+        self.date_slot = {}
+        self.book_tables = {}
+
+    
+    def add_time_slot(self, time_slot, availability=True):
+        self.time_slot[time_slot] = availability
+
+    def add_date_slot(self, date, availability=True):
+        self.date_slot[date] = availability 
+    
+    def book_tables(self, table_number):
+        self.book_table.append(table_number) 
+    
+restaurant = Restaurant()
